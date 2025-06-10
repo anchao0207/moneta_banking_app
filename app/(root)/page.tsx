714f5601a55fefd6beaa3c98f832f5debf8216f2
An Trieu <anchao0207@gmail.com>
2025-06-10 22:55:12 +0000

    implement sidebars (right and left)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "An" };
+  const loggedIn = {
+    firstName: "An",
+    lastName: "Trieu",
+    email: "an@abccomputech.com",
+  };
   return (
     <section className="home">
       <div className="home-content">
@@ -21,7 +26,13 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500 }]}
+      />
     </section>
   );
 };
