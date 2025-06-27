34fe5585ad6ac4f9b8b8d50aff474613f12d1a48
An Trieu <anchao0207@gmail.com>
2025-06-27 21:47:00 +0000

    implement transfer page and function

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -1,6 +1,7 @@
 import { formatAmount } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import Copy from "./Copy";
 
 const BankCard = ({
   account,
@@ -9,7 +10,10 @@ const BankCard = ({
 }: CreditCardProps) => {
   return (
     <div className="flex flex-col">
-      <Link href="/" className="bank-card">
+      <Link
+        href={`/transaction-history/${account?.appwriteItemId}`}
+        className="bank-card"
+      >
         <div className="bank-card_content">
           <div>
             <h1 className="text-16 font-semibold text-white">{userName}</h1>
@@ -28,7 +32,7 @@ const BankCard = ({
             <p className="text-14 font-semibold tracking-[1.1px] text-white">
               &#9679;&#9679;&#9679;&#9679; &#9679;&#9679;&#9679;&#9679;
               &#9679;&#9679;&#9679;&#9679;{" "}
-              <span className="text-16">{1234}</span>
+              <span className="text-16">{account?.mask}</span>
             </p>
           </article>
         </div>
@@ -52,7 +56,7 @@ const BankCard = ({
         </div>
       </Link>
 
-      {/* COPY */}
+      {showBalance && <Copy title={account?.sharableId} />}
     </div>
   );
 };
