caabc07a885ac089877e1c98db282e71dc28825b
An Trieu <anchao0207@gmail.com>
2025-06-30 20:48:19 +0000

    implement pagination for transactions

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -8,9 +8,7 @@ Sentry.init({
   dsn: "https://d144718c31a0a3b72d78a4a547010609@o4509492500561920.ingest.us.sentry.io/4509492502134784",
 
   // Add optional integrations for additional features
-  integrations: [
-    Sentry.replayIntegration(),
-  ],
+  integrations: [Sentry.replayIntegration()],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
